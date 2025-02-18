68c578ab53b32a7594acf4c4508fe11a5b65e865
Soph Zeng <sadgebab@gmail.com>
2025-02-17 16:11:05 -0800

    Added additional functions to the Node class and wrote two PseudoNode classes for testing, added tests

--- src/node.py
+++ src/node.py
@@ -5,25 +5,22 @@ import abc
 import uuid
 from typing import Optional, Tuple, List
 
-class POSITION(Enum):
-    FRONT = 1   
-    BACK = 2
-
 class Node(abc.ABC):
     """
     Abstract Node class in a Linked-List 
     """
     def __init__(
         self,
-        name: str = "",
+        type: str = "",
+        params: dict = {},
         in_shape: Optional[Tuple[int, ...]] = None,
         out_shape: Optional[Tuple[int, ...]] = None,
         input_node: Optional["Node"] = None,
         output_node: Optional["Node"] = None,
-    ): 
+    ):
         self.id = str(uuid.uuid4())  # Unique ID
-        self.name = name or f"Node_{self.id}"
-        # Instead of in_dim/out_dim, we store a shape tuple
+        self.type= type or f"Node_{self.id}"
+        self.params = params
         self.in_shape: Optional[Tuple[int, ...]] = in_shape
         self.out_shape: Optional[Tuple[int, ...]] = out_shape
 
@@ -45,44 +42,28 @@ class Node(abc.ABC):
         self.input_node: Optional["Node"] = input_node
         self.output_node: Optional["Node"] = output_node
     
-    @abc.abstractmethod
+    
     def __str__(self) -> str:
         """
         Return a human-readable summary of the node (e.g. 'Linear((128,), (64,))').
         """
-        pass
+        params_str = ", ".join(f"{param}={val}" for param, val in self.params.items())
+        return f"{self.type}({params_str})"
 
 
-    @abc.abstractmethod
     def get_layer_type(self) -> str:
         """
         Return a string with the layer name.
         """
-        pass
+        return self.type
 
-    @abc.abstractmethod
+    
     def get_layer_params(self) -> dict:
         """
         Return a dict with the layer params.
         """
-        pass
- 
- 
-    @abc.abstractmethod
-    def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
-        """
-        infer out_shape from in_shape with layer type and layer parameters
-        """
-        pass
-
-    @abc.abstractmethod
-    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
-        """
-        infer in_shape from out_shape with layer type and layer parameters
-        """
-        pass
-
-    @abc.abstractmethod
+        return self.params 
+    
     def check_shape_validity(self, shape: Tuple[int, ...]) -> str:
         """
         check if a shape is a valid tensor shape
@@ -94,7 +75,7 @@ class Node(abc.ABC):
                 raise ValueError("invalid shape {shape}: {i}-th dim {shape[i]} must be larger than 0") 
         return 
     
-    @abc.abstractmethod
+    
     def get_input_node(self) -> 'Node':
         """
         Return the input node of the current node
@@ -103,7 +84,7 @@ class Node(abc.ABC):
             raise ValueError("Input node is None")
         return self.input_node
     
-    @abc.abstractmethod
+    
     def get_output_node(self) -> 'Node':
         """
         Return the output node of the current node
@@ -112,7 +93,6 @@ class Node(abc.ABC):
             raise ValueError("Output node is None")
         return self.output_node
 
-    @abc.abstractmethod
     def set_input_node(self, other_node: 'Node'):
         """
         Link a node to the input of this node 
@@ -124,9 +104,10 @@ class Node(abc.ABC):
                 raise ValueError("Invalid add node, link node {self} to the output of {other_node}, {self} in_shape mismatch with {other_node} out_shape")
         # all things check out, connect the two nodes
         self.input_node = other_node
-        other_node.get_output_node = self # check correctness, check whether using a getter is a good way of doing things 
-
-    @abc.abstractmethod        
+        other_node.get_output_node = self # TODO check correctness, check whether using a getter is a good way of doing things 
+        #because we already checked for the validity of linking other_node to the input. we can directly set the output_node of 
+        #other node to this node 
+    
     def set_output_node(self, other_node: 'Node'): 
         """
         Link a node to the output of this node 
@@ -137,14 +118,15 @@ class Node(abc.ABC):
             if self.out_shape != other_node.in_shape: # in_shape assigned and mismatched with prev node"s out_shape
                 raise ValueError("Invalid add node, link node {self} to the input of {other_node}, {self} out_shape mismatch with {other_node} in_shape")
         # all things check out, connect the two nodes
-        self.output_node = other_node
-        other_node.get_input_node = self # check correctness, check whether using a getter is a good way of doing things 
+        self.output_node = other_node 
+        other_node.get_input_node = self # TODO check correctness, check whether using a getter is a good way of doing things 
+        #because we already checked for the validity of linking other_node to the output. we can directly set the input_node of 
+        #other node to this node 
+
     # Note: connecting two nodes with the current node consists of two steps:
     # 1. adding the current node to the back of the prev node, calling set_input_node
     # 2. adding the current node to the front of the next node, calling set_output_node
 
-
-    @abc.abstractmethod
     def set_in_shape(self, new_in_shape) -> 'Node':
         """
         Set the input shape.
@@ -164,8 +146,8 @@ class Node(abc.ABC):
             raise ValueError(f"Failed to infer out_shape from the new in_shape {new_in_shape}: {str(e)}")
         self.in_shape = new_in_shape
         return self
+     
     
-    @abc.abstractmethod
     def set_out_shape(self, new_out_shape) -> 'Node':
         """
         Set the output shape.
@@ -187,17 +169,79 @@ class Node(abc.ABC):
         return self
     
     @abc.abstractmethod
+    def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
+        """
+        infer out_shape from in_shape with layer type and layer parameters
+        """
+        pass
+
+    @abc.abstractmethod
+    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
+        """
+        infer in_shape from out_shape with layer type and layer parameters
+        """
+        pass
+    
+    @abc.abstractmethod
     def to_torch(self) -> str:
         """
         Return a string with the PyTorch layer construction (e.g. 'nn.Linear(128, 64)').
         """
         pass
-    
 
-class PseudoNode(Node):
+class PseudoNode_second_dim_multiply(Node):
     """
     This Class exists for testing the functionalities of the Node Class 
     """
-    
+    def __init__(self, factor=1):
+        super().__init__(type="pseudoNode_second_dim_divide", param={'factor': factor})
+
+    def __str__(self) -> str:
+        return f"PseudoNode_second_dim_multiply(in_shape={self.in_shape}, out_shape={self.out_shape})"
+
+    def get_layer_type(self) -> str:
+        return self.name
+
+    def get_layer_params(self) -> dict:
+        return {}
+
+    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
+        if len(out_shape) < self.params['factor'] or out_shape[1] % self.params['factor'] != 0:
+            raise ValueError("The second dimension must be even and at least 2.")
+        return (out_shape[0], out_shape[1] // self.params['factor']) + out_shape[self.params['factor']:]
+
+    def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
+        return (in_shape[0], in_shape[1] * self.params['factor']) + in_shape[self.params['factor']:]
+
+    def to_torch(self) -> str:
+        return "PseudoLayer_second_dim_multiply()"
 
 
+
+class PseudoNode_second_dim_divide(Node):
+    """
+    This Class exists for testing the functionalities of the Node Class 
+    """
+    def __init__(self, factor=1):
+        super().__init__(type="pseudoNode_second_dim_divide", params={'factor': factor})
+        
+
+    def __str__(self) -> str:
+        return f"PseudoNode_second_dim_divide(in_shape={self.in_shape}, out_shape={self.out_shape})"
+
+    def get_layer_type(self) -> str:
+        return "PseudoNode_second_dim_divide"
+
+    def get_layer_params(self) -> dict:
+        return {}
+
+    def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
+        if len(in_shape) < self.params['factor'] or in_shape[1] % self.params['factor'] != 0:
+            raise ValueError("The second dimension must be even and at least 2.")
+        return (in_shape[0], in_shape[1] // self.params['factor']) + in_shape[self.params['factor']:]
+
+    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
+        return (out_shape[0], out_shape[1] * self.params['factor']) + out_shape[self.params['factor']:]
+
+    def to_torch(self) -> str:
+        return "PseudoLayer_second_dim_multiply()"
