21b8d885053efcfbcf59c74dac21829439baa970
Soph Zeng <sadgebab@gmail.com>
2025-02-18 12:42:30 -0800

    Removed backward_dimension_inference

--- src/node.py
+++ src/node.py
@@ -5,6 +5,11 @@ import abc
 import uuid
 from typing import Optional, Tuple, List
 
+
+#Some notes to front-end: 
+#the bubble sould only look green if a node has in_shape, out_shape, input_node and output_node all defined
+#call completed() to check 
+
 class Node(abc.ABC):
     """
     Abstract Node class in a Linked-List 
@@ -30,14 +35,14 @@ class Node(abc.ABC):
             except (NotImplementedError, ValueError) as e:
                 raise ValueError(f"Failed to infer out_shape: {str(e)}") 
             
-        if self.in_shape is None and self.out_shape is not None:
-            try:
-                self.in_shape = self.backward_dimension_inference()
-            except (NotImplementedError, ValueError) as e:
-                raise ValueError(f"Failed to infer in_shape: {str(e)}")
+        # if self.in_shape is None and self.out_shape is not None:
+        #     try:
+        #         self.in_shape = self.backward_dimension_inference()
+        #     except (NotImplementedError, ValueError) as e:
+        #         raise ValueError(f"Failed to infer in_shape: {str(e)}")
             
-        if self.in_shape is None and self.out_shape is None:
-            raise ValueError(f"in_shape and out_shape cannot both be None")
+        #if self.in_shape is None and self.out_shape is None:
+        #    raise ValueError(f"in_shape and out_shape cannot both be None")
             
         self.input_node: Optional["Node"] = input_node
         self.output_node: Optional["Node"] = output_node
@@ -49,7 +54,10 @@ class Node(abc.ABC):
         """
         params_str = ", ".join(f"{param}={val}" for param, val in self.params.items())
         return f"{self.type}({params_str})"
-
+    
+    def completed(self):
+        return self.in_shape != None and self.out_shape != None and self.input_node != None and self.output_node != None 
+    
 
     def get_layer_type(self) -> str:
         """
@@ -96,7 +104,7 @@ class Node(abc.ABC):
 
     def set_input_node(self, other_node: 'Node'):
         """
-        Link a node to the input of this node 
+        Link a node to the input of this node, throw error if the other_node's out_shape is defined and mismatch with this node's in_shape
         """
         if self.in_shape == None:
             self.set_in_shape(other_node.out_shape) # this call will check if with the new in_shape, the node is still valid 
@@ -105,14 +113,14 @@ class Node(abc.ABC):
                 raise ValueError("Invalid add node, link node {self} to the output of {other_node}, {self} in_shape mismatch with {other_node} out_shape")
         # all things check out, connect the two nodes
         self.input_node = other_node
-        other_node.get_output_node = self # TODO check correctness, check whether using a getter is a good way of doing things 
+        other_node.output_node = self 
         #because we already checked for the validity of linking other_node to the input. we can directly set the output_node of 
         #other node to this node 
     
 
     def set_output_node(self, other_node: 'Node'): 
         """
-        Link a node to the output of this node 
+        Link a node to the output of this node, throw error if the other_node's in_shape is defined and mismatch with this node's out_shape
         """
         if self.out_shape == None:   
             self.set_out_shape(other_node.in_shape) # this call will check if with the new out_shape, the node is still valid 
@@ -121,7 +129,7 @@ class Node(abc.ABC):
                 raise ValueError("Invalid add node, link node {self} to the input of {other_node}, {self} out_shape mismatch with {other_node} in_shape")
         # all things check out, connect the two nodes
         self.output_node = other_node 
-        other_node.get_input_node = self # TODO check correctness, check whether using a getter is a good way of doing things 
+        other_node.input_node = self 
         #because we already checked for the validity of linking other_node to the output. we can directly set the input_node of 
         #other node to this node 
 
@@ -147,29 +155,36 @@ class Node(abc.ABC):
         except (NotImplementedError, ValueError) as e:
             raise ValueError(f"Failed to infer out_shape from the new in_shape {new_in_shape}: {str(e)}")
         self.in_shape = new_in_shape
-        return self
+        return 
      
     
     def set_out_shape(self, new_out_shape) -> 'Node':
         """
         Set the output shape.
-        Then call the backward_dimension_inference 
-        to check if the out_shape is valid for this layer type 
-        and whether the resulting in_shape matches the predefined in_shape
+        Only check if the out_shape is valid 
+        Does not apply dimensional inference to retrieve the in-shape 
+        As in many cases with nn.modules such as Conv2D and tensor operations such as Reshape backward dimension inference is not impossible 
+        TODO The out_shape can be set if in-shape is None. 
+        When we set in-shape in the set_in_shape call in the future we will check if the defined out_shape matches the inferred out_shape 
+        TODO If in_shape is already defined, we will do a forward_dimension_inference to check if it is valid. 
         """
         self.validate_shape(new_out_shape) ##TODO wrap it in a try so we can raise more meaningful errors
-        try:
-            new_in_shape = self.backward_dimension_inference(new_out_shape)
-            if self.in_shape is not None and new_in_shape != self.in_shape:
-                raise ValueError(f"New out_shape {new_out_shape} requires in_shape {new_in_shape} that doesn't match existing in_shape {self.in_shape}")
-            if self.out_shape is None: 
-                self.validate_shape(new_in_shape) ##TODO wrap it in a try so we can raise more meaningful errors
-                self.in_shape = new_in_shape 
-        except (NotImplementedError, ValueError) as e:
-            raise ValueError(f"Failed to infer in_shape from the new out_shape {new_out_shape}: {str(e)}")
-        self.out_shape = new_out_shape 
-        return self
+        if self.in_shape is None:
+            self.out_shape = new_out_shape
+        else: 
+            try:
+                out_shape = self.forward_dimension_inference(self.in_shape)
+                if out_shape != new_out_shape:
+                # This error is not supposed to be reached, we put it here for robustness 
+                    raise ValueError(f"out_shape {new_out_shape} does not match the out_shape {out_shape} inferred from the existing in_shape{self.in_shape}")
+            except (NotImplementedError, ValueError) as e:
+                # This error is not supposed to be reached, we put it here for robustness 
+                raise ValueError(f"While setting new out shape {new_out_shape}, failed to infer out_shape from the in_shape {self.in_shape}: {str(e)}. Check if the current in_shape{self.in_shape} and layer parameters are correct.")
+            self.out_shape = new_out_shape
+        return 
     
+    
+
     @abc.abstractmethod
     def validate_params(self):
         """
@@ -181,13 +196,9 @@ class Node(abc.ABC):
     def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
         """
         infer out_shape from in_shape with layer type and layer parameters
-        """
-        pass
-
-    @abc.abstractmethod
-    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
-        """
-        infer in_shape from out_shape with layer type and layer parameters
+        cfcf vb TODO  in some cases, the layer parameters already defined the out_shape, like Reshape 
+        in these cases, we check if the defined operations can be applied on this in_shape, and throw errors if it cannot be,
+        if it can be, it returns the predefined out_shape 
         """
         pass
     
