72d6879def51850c3300d26d92147f1a72682895
Soph Zeng <sadgebab@gmail.com>
2025-02-19 17:52:25 -0800

    Optimized the Node(Abstract) class and implemented Tensor and Reshape class.

--- src/node.py
+++ src/node.py
@@ -3,16 +3,16 @@ from enum import Enum
 import torch
 import abc
 import uuid
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, override
+import tensorflow as tf 
+import numpy as np 
 
-
-#Some notes to front-end: 
-#the bubble sould only look green if a node has in_shape, out_shape, input_node and output_node all defined
-#call completed() to check 
 
 class Node(abc.ABC):
     """
-    Abstract Node class in a Linked-List 
+    Abstract Node class 
+    An abstraction for all layers and aggregated NN-graph 
+    In the form a Linked-list Node 
     """
     def __init__(
         self,
@@ -104,34 +104,27 @@ class Node(abc.ABC):
 
     def set_input_node(self, other_node: 'Node'):
         """
-        Link a node to the input of this node, throw error if the other_node's out_shape is defined and mismatch with this node's in_shape
+        Link a node to the input of this node, throw error if the other_node's out_shape is defined and mismatched with this node's in_shape
         """
-        if self.in_shape == None:
-            self.set_in_shape(other_node.out_shape) # this call will check if with the new in_shape, the node is still valid 
-        else:
-            if self.in_shape != other_node.out_shape: # in_shape assigned and mismatched with prev out_shape
-                raise ValueError("Invalid add node, link node {self} to the output of {other_node}, {self} in_shape mismatch with {other_node} out_shape")
+        if self.in_shape is None:
+            raise ValueError("Tensor in_shape should never be None.")
+        if self.in_shape != other_node.out_shape: # in_shape assigned and mismatched with prev out_shape
+            raise ValueError("Invalid add node, link node {self} to the output of {other_node}, {self} in_shape mismatch with {other_node} out_shape")
         # all things check out, connect the two nodes
         self.input_node = other_node
         other_node.output_node = self 
-        #because we already checked for the validity of linking other_node to the input. we can directly set the output_node of 
-        #other node to this node 
-    
 
-    def set_output_node(self, other_node: 'Node'): 
+    def set_output_node(self, other_node: 'Node'):
         """
-        Link a node to the output of this node, throw error if the other_node's in_shape is defined and mismatch with this node's out_shape
+        Link a node to the output of this node, throw error if the other_node's in_shape is defined and mismatched with this node's out_shape
         """
-        if self.out_shape == None:   
-            self.set_out_shape(other_node.in_shape) # this call will check if with the new out_shape, the node is still valid 
-        else: 
-            if self.out_shape != other_node.in_shape: # in_shape assigned and mismatched with prev node"s out_shape
-                raise ValueError("Invalid add node, link node {self} to the input of {other_node}, {self} out_shape mismatch with {other_node} in_shape")
+        if self.out_shape is None:
+            raise ValueError("Tensor out_shape should never be None.")
+        if self.out_shape != other_node.in_shape: # in_shape assigned and mismatched with prev node's out_shape
+            raise ValueError("Invalid add node, link node {self} to the input of {other_node}, {self} out_shape mismatch with {other_node} in_shape")
         # all things check out, connect the two nodes
         self.output_node = other_node 
-        other_node.input_node = self 
-        #because we already checked for the validity of linking other_node to the output. we can directly set the input_node of 
-        #other node to this node 
+        other_node.input_node = self
 
     # Note: connecting two nodes with the current node consists of two steps:
     # 1. adding the current node to the back of the prev node, calling set_input_node
@@ -182,8 +175,6 @@ class Node(abc.ABC):
                 raise ValueError(f"While setting new out shape {new_out_shape}, failed to infer out_shape from the in_shape {self.in_shape}: {str(e)}. Check if the current in_shape{self.in_shape} and layer parameters are correct.")
             self.out_shape = new_out_shape
         return 
-    
-    
 
     @abc.abstractmethod
     def validate_params(self):
@@ -196,7 +187,7 @@ class Node(abc.ABC):
     def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
         """
         infer out_shape from in_shape with layer type and layer parameters
-        cfcf vb TODO  in some cases, the layer parameters already defined the out_shape, like Reshape 
+        TODO  in some cases, the layer parameters already defined the out_shape, like Reshape 
         in these cases, we check if the defined operations can be applied on this in_shape, and throw errors if it cannot be,
         if it can be, it returns the predefined out_shape 
         """
@@ -210,64 +201,125 @@ class Node(abc.ABC):
         pass
 
 
+class Tensor(Node):
+    """
+    Represents a tensor node
+    It's usually defined as an input and output node of a Seq 
+    """
+    def __init__(self, data: torch.Tensor| tf.Tensor | np.ndarray):
+        super().__init__(type="Tensor", in_shape=data.shape , out_shape=data.shape) # The in_shape and out_shape of a Tensor type equals to its data shape. It is immutable and will never be None once the Node is defined. 
+        self.data = data
+
+    def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
+        """
+        For a tensor, the output shape is the same as the input shape.
+        """ 
+        return in_shape 
+
+    def to_torch(self) -> str:
+        """
+        Return a string with the PyTorch tensor construction.
+        Convert self.data to torch.Tensor if it is not already.
+        """
+        if isinstance(self.data, torch.Tensor):
+            return f"torch.tensor({self.data.tolist()})"
+        elif isinstance(self.data, tf.Tensor):
+            data = torch.tensor(self.data.numpy())
+        elif isinstance(self.data, np.ndarray):
+            data = torch.tensor(self.data)
+        else:
+            raise TypeError("Unsupported data type for conversion to torch.Tensor")
+        return f"torch.tensor({data.tolist()})"
+
+    def to_tf(self) -> str:
+        """
+        Return a string with the TensorFlow tensor construction.
+        Convert self.data to tf.Tensor if it is not already.
+        """
+        if isinstance(self.data, tf.Tensor):
+            return f"tf.convert_to_tensor({self.data.numpy().tolist()})"
+        elif isinstance(self.data, torch.Tensor):
+            data = tf.convert_to_tensor(self.data.numpy())
+        elif isinstance(self.data, np.ndarray):
+            data = tf.convert_to_tensor(self.data)
+        else:
+            raise TypeError("Unsupported data type for conversion to tf.Tensor")
+        return f"tf.convert_to_tensor({data.numpy().tolist()})"
+
+    def validate_params(self):
+        """
+        Validate that the tensor data is consistent with the shape.
+        """
+        if self.data.shape != self.in_shape:
+            raise ValueError("Tensor data shape does not match the specified in_shape")
+    
+    @override
+    def set_in_shape(self, new_in_shape) -> Node:
+        raise ValueError("Invalid Call. Cannot change in_shape of a Tensor. In_shape is immutable and must match the data shape. Consider creating a new Tensor")
+
+    @override
+    def set_out_shape(self, new_out_shape) -> Node:
+        raise ValueError("Invalid Call. Cannot change out_shape of a Tensor. Out_shape is immutable and must match the data shape. Consider creating a new Tensor")
+    
+    @override
+    def set_input_node(self, other_node: 'Node'):
+        """
+        Link a node to the input of this node, throw error if the other_node's out_shape is defined and mismatched with this node's in_shape
+        """
+        if self.in_shape is None:
+            raise ValueError("Tensor in_shape should never be None.")
+        if self.in_shape != other_node.out_shape: # in_shape assigned and mismatched with prev out_shape
+            raise ValueError("Invalid add node, link node {self} to the output of {other_node}, {self} in_shape mismatch with {other_node} out_shape")
+        # all things check out, connect the two nodes
+        self.input_node = other_node
+        other_node.output_node = self
+
+    @override
+    def set_output_node(self, other_node: 'Node'):
+        """
+        Link a node to the output of this node, throw error if the other_node's in_shape is defined and mismatched with this node's out_shape
+        """
+        if self.out_shape is None:
+            raise ValueError("Tensor out_shape should never be None.")
+        if self.out_shape != other_node.in_shape: # in_shape assigned and mismatched with prev node's out_shape
+            raise ValueError("Invalid add node, link node {self} to the input of {other_node}, {self} out_shape mismatch with {other_node} in_shape")
+        # all things check out, connect the two nodes
+        self.output_node = other_node
+        other_node.input_node = self
+
+
 class Reshape(Node):
     """
     Maps to reshape in Torch 
     """
-    def __init__(self, out_dim = [-1, ]):
-        super().__init__(type="Reshape", params= {"out_dim": [-1, ]})
+    def __init__(self, out_dim):
+        super().__init__(type="Reshape", params= {"out_dim": out_dim})
 
     def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
-        factor = self.params['factor']
-        return (in_shape[0], in_shape[1] * factor) + in_shape[factor:]
-
-    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
-        factor = self.params['factor']
-        if len(out_shape) < factor or out_shape[1] % factor != 0:
-            raise ValueError("The second dimension must be at least {factor} and divisible by {factor}")
-        return (out_shape[0], out_shape[1] // factor) + out_shape[factor:]
-
-    def to_torch(self) -> str:
-        return "PseudoLayer_second_dim_multiply()"
-
-
-class PseudoNode_second_dim_multiply(Node):
-    """
-    This Class exists for testing the functionalities of the Node Class 
-    """
-    def __init__(self, factor=1):
-        super().__init__(type="PseudoNode_second_dim_multiply", params={'factor': factor})
-
-    def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
-        factor = self.params['factor']
-        return (in_shape[0], in_shape[1] * factor) + in_shape[factor:]
-
-    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
-        factor = self.params['factor']
-        if len(out_shape) < factor or out_shape[1] % factor != 0:
-            raise ValueError("The second dimension must be at least {factor} and divisible by {factor}")
-        return (out_shape[0], out_shape[1] // factor) + out_shape[factor:]
+        out_dim = self.params['out_dim']
+        if not isinstance(out_dim, tuple):
+            raise ValueError("out_dim must be a tuple")
+        in_elements = torch.prod(torch.tensor(in_shape)).item()
+        out_elements = 1
+        inferred_index = -1
+        for i, dim in enumerate(out_dim):
+            if dim == -1:
+                if inferred_index != -1:
+                    raise ValueError("Only one dimension can be inferred, redefine the reshape parameters to contain only one -1")
+                inferred_index = i
+            else:
+                out_elements *= dim
+        if inferred_index != -1:
+            if in_elements % out_elements != 0:
+                raise ValueError("Cannot infer dimension: total elements do not match")
+            inferred_dim = in_elements // out_elements
+            out_dim = out_dim[:inferred_index] + (inferred_dim,) + out_dim[inferred_index+1:]
+        elif in_elements != out_elements:
+            raise ValueError(f"Cannot reshape from {in_shape} to {out_dim}: total elements do not match")
+        return out_dim
 
     def to_torch(self) -> str:
-        return "PseudoLayer_second_dim_multiply()"
+        out_dim = self.params['out_dim']
+        return f"torch.reshape(input, {out_dim})"
+    
 
-
-class PseudoNode_second_dim_divide(Node):
-    """
-    This Class exists for testing the functionalities of the Node Class 
-    """
-    def __init__(self, factor=1):
-        super().__init__(type="PseudoNode_second_dim_divide", params={'factor': factor}) 
-
-    def forward_dimension_inference(self, in_shape) -> Tuple[int, ...]:
-        factor = self.params['factor'] 
-        if len(in_shape) < factor or in_shape[1] % factor != 0:
-            raise ValueError("The second dimension must be even and at least {factor} and be divisible by {factor}")
-        return (in_shape[0], in_shape[1] / factor) + in_shape[factor:]
-
-    def backward_dimension_inference(self, out_shape) -> Tuple[int, ...]:
-        factor = self.params['factor'] 
-        return (out_shape[0], out_shape[1] * self.params['factor']) + out_shape[factor:]
-
-    def to_torch(self) -> str:
-        return "PseudoLayer_second_dim_multiply()"
--- src/node_tests.py
+++ src/node_tests.py
@@ -1,30 +1,12 @@
-from node import Node, PseudoNode_second_dim_divide, PseudoNode_second_dim_multiply
+from node import Node, Reshape, Tensor
 import unittest
 
-class TestPseudoNode(unittest.TestCase):
-    def test_multiply_forward_inference(self):
-        node = PseudoNode_second_dim_multiply(factor=2)
-        in_shape = (1, 4, 2)
-        expected_out_shape = (1, 8, 2)
-        self.assertEqual(node.forward_dimension_inference(in_shape), expected_out_shape)
 
-    def test_multiply_backward_inference(self):
-        node = PseudoNode_second_dim_multiply(factor=2)
-        out_shape = (1, 8, 2)
-        expected_in_shape = (1, 4, 2)
-        self.assertEqual(node.backward_dimension_inference(out_shape), expected_in_shape)
+#Some notes to front-end: 
+#the bubble sould only look green if a node has in_shape, out_shape, input_node and output_node all defined
+#call .completed() to check
 
-    def test_divide_forward_inference(self):
-        node = PseudoNode_second_dim_divide(factor=2)
-        in_shape = (1, 8, 2)
-        expected_out_shape = (1, 4, 2)
-        self.assertEqual(node.forward_dimension_inference(in_shape), expected_out_shape)
 
-    def test_divide_backward_inference(self):
-        node = PseudoNode_second_dim_divide(factor=2)
-        out_shape = (1, 4, 2)
-        expected_in_shape = (1, 8, 2)
-        self.assertEqual(node.backward_dimension_inference(out_shape), expected_in_shape)
 
 if __name__ == '__main__':
     unittest.main()
