7889beef3229aa116047b0766703e565b5b18881
Soph Zeng <sadgebab@gmail.com>
2025-02-20 02:53:10 -0800

    Added more unit tests with cases

--- src/node_tests.py
+++ src/node_tests.py
@@ -52,6 +52,50 @@ class TestNodeOperations(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             reshape_node.set_output_node(different_tensor)
         self.assertIn("out_shape mismatch", str(context.exception))
+    
+    def test_swap_layer(self):
+        # Create a tensor with shape (3, 8, 3)
+        input = torch.randn(3, 8, 3)
+        input_node = Tensor(input)
+        
+        # Apply reshape (3, 4, -1)
+        reshape_node1 = Reshape((3, 4, -1))
+        reshape_node1.set_input_node(input_node)
+        self.assertEqual(reshape_node1.out_shape, (3, 4, 6))
+
+        # Apply reshape (3, -1, 2)
+        reshape_node2 = Reshape((3, -1, 2))
+        reshape_node2.set_input_node(reshape_node1)
+        self.assertEqual(reshape_node2.out_shape, (3, 12, 2))
+
+        output = torch.randn(3, 12, 2)
+        output_node = Tensor(output)
+        output_node.set_input_node(reshape_node2) 
+
+        # Now you have Input Tensor - Reshape(3, 4, -1) - Reshape(3, -1, 2) = Output Tensor 
+        # We want to swap Reshape (3, 4, -1) to Reshape (3, 8, 2)
+        reshape_node1_1 = Reshape((3, 8, 2))
+        with self.assertRaises(ValueError) as context:
+            reshape_node1_1.set_input_node(input_node) # cannot set input node here because you cannot reshape (3, 8, 3) to (3, 8, 2)
+            reshape_node1_1.set_output_node(reshape_node2)
+        self.assertIn("Failed to infer", str(context.exception))
+
+        # Then we want to swap Reshape (3, 4, -1) to Reshape (3, 2, 12) this would be okay but we cannot reconnect anymore with the following layer
+        reshape_node1_2 = Reshape((3, 2, 12))
+        with self.assertRaises(ValueError) as context:
+            reshape_node1_2.set_input_node(input_node) 
+            reshape_node1_2.set_output_node(reshape_node2)
+        self.assertIn("Invalid add node", str(context.exception)) 
+
+        # Then we want to swap Reshape (3, 4, -1) to Reshape (3, 2, 12) this would be okay but we cannot reconnect anymore with the following layer
+        reshape_node1_2 = Reshape((3, 2, 12))
+        with self.assertRaises(ValueError) as context:
+            reshape_node1_2.set_input_node(input_node) 
+            reshape_node1_2.set_output_node(reshape_node2)
+        self.assertIn("Invalid add node", str(context.exception))
+        # This step would essentially invalidate all the downstream connections 
+
+        # Next step I will to infer the reshapes  
 
 if __name__ == '__main__':
     unittest.main()
